738fb72f122d13ef3f5984e70c10a35eeaf06cde
akhdnn <heroesakhdan@gmail.com>
2026-03-15 11:32:03 +0700

    update script

--- update-web.sh
+++ update-web.sh
@@ -5,4 +5,8 @@ git pull origin main
 echo "Memulai build ulang container Docker (hanya service app)..."
 docker compose up -d --build app
 
+echo "Membersihkan cache Docker..."
+docker image prune -f
+docker builder prune -f
+
 echo "Selesai! Silakan cek web kamu."
