4e1e0b33f929bdee1058107ab0035c8db36b9c07
akhdnn <heroesakhdan@gmail.com>
2026-03-15 10:36:14 +0700

    refactor: ensure dotenv is loaded for Prisma client and conditionally in Prisma configuration

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,17 @@
-import "dotenv/config";
-import { defineConfig } from "prisma/config";
+try {
+  require("dotenv/config");
+} catch (e) {
+  // dotenv tidak ditemukan di lingkungan produksi Docker, 
+  // tapi tidak apa-apa karena variabel lingkungan sudah disediakan oleh Docker/Compose
+}
 
-export default defineConfig({
+/** @type {import('prisma/config').PrismaConfig} */
+export default {
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env.DATABASE_URL as string,
+    url: process.env.DATABASE_URL,
   },
-});
+};
